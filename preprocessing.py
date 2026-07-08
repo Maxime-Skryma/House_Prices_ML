09e8f61545eab4e6afccbcb6c5a6c7886447b3f0
maximeskryma <maxime.skryma@ensae.fr>
2026-07-08 09:08:41 +0000

    Création Pipeline tranformers

--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,43 @@ from sklearn.impute import SimpleImputer
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 
+# Échelle de qualité générique (revient sur 6 colonnes)
+qual_scale = ['Po', 'Fa', 'TA', 'Gd', 'Ex']
+
+# Variantes avec "pas d'objet" (NA) en plus, à placer selon ton choix
+# (toi tu as déjà remplacé les NA par 'None' plus tôt dans la conversation)
+qual_scale_with_none = ['None', 'Po', 'Fa', 'TA', 'Gd', 'Ex']
+
+# PoolQC n'a pas de modalité 'Po' dans le fichier
+poolqc_scale = ['None', 'Fa', 'TA', 'Gd', 'Ex']
+
+ordinal_scales = {
+    'ExterQual':    qual_scale_with_none,   # pas de NA en réalité, mais safe
+    'ExterCond':    qual_scale_with_none,
+    'HeatingQC':    qual_scale_with_none,
+    'KitchenQual':  qual_scale_with_none,
+    'BsmtQual':     qual_scale_with_none,   # NA = No Basement
+    'GarageQual':   qual_scale_with_none,   # NA = No Garage
+    'GarageCond':   qual_scale_with_none,   # NA = No Garage
+    'FireplaceQu':  qual_scale_with_none,   # NA = No Fireplace
+    'PoolQC':       poolqc_scale,           # NA = No Pool, pas de 'Po'
+
+    'BsmtExposure': ['None', 'No', 'Mn', 'Av', 'Gd'],           # NA = No Basement
+    'BsmtFinType1': ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'],  # NA = No Basement
+    'BsmtFinType2': ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'],
+
+    'GarageFinish': ['None', 'Unf', 'RFn', 'Fin'],              # NA = No Garage
+
+    'Utilities':   ['None', 'ELO', 'NoSeWa', 'NoSewr', 'AllPub'],
+    'LotShape':    ['None', 'Reg', 'IR1', 'IR2', 'IR3'],
+    'LandSlope':   ['None', 'Gtl', 'Mod', 'Sev'],
+    'PavedDrive':  ['None', 'N', 'P', 'Y'],
+    'Functional':  ['None', 'Sal', 'Sev', 'Maj2', 'Maj1', 'Mod', 'Min2', 'Min1', 'Typ'],
+
+
+    'Fence':        ['None', 'MnWw', 'GdWo', 'MnPrv', 'GdPrv'],  # ordre discutable, cf. remarque
+}
+
 def preprocessing(df,power_transformer=None,imputer=None):
     df = df.drop(columns=['Id'])
     
@@ -21,43 +58,6 @@ def preprocessing(df,power_transformer=None,imputer=None):
     df = df.drop(columns=['PoolArea'])
 
     #Modification des variables catégorielles qui sont en fait ordinales
-
-    # Échelle de qualité générique (revient sur 6 colonnes)
-    qual_scale = ['Po', 'Fa', 'TA', 'Gd', 'Ex']
-
-    # Variantes avec "pas d'objet" (NA) en plus, à placer selon ton choix
-    # (toi tu as déjà remplacé les NA par 'None' plus tôt dans la conversation)
-    qual_scale_with_none = ['None', 'Po', 'Fa', 'TA', 'Gd', 'Ex']
-
-    # PoolQC n'a pas de modalité 'Po' dans le fichier
-    poolqc_scale = ['None', 'Fa', 'TA', 'Gd', 'Ex']
-
-    ordinal_scales = {
-        'ExterQual':    qual_scale_with_none,   # pas de NA en réalité, mais safe
-        'ExterCond':    qual_scale_with_none,
-        'HeatingQC':    qual_scale_with_none,
-        'KitchenQual':  qual_scale_with_none,
-        'BsmtQual':     qual_scale_with_none,   # NA = No Basement
-        'GarageQual':   qual_scale_with_none,   # NA = No Garage
-        'GarageCond':   qual_scale_with_none,   # NA = No Garage
-        'FireplaceQu':  qual_scale_with_none,   # NA = No Fireplace
-        'PoolQC':       poolqc_scale,           # NA = No Pool, pas de 'Po'
-
-        'BsmtExposure': ['None', 'No', 'Mn', 'Av', 'Gd'],           # NA = No Basement
-        'BsmtFinType1': ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'],  # NA = No Basement
-        'BsmtFinType2': ['None', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'],
-
-        'GarageFinish': ['None', 'Unf', 'RFn', 'Fin'],              # NA = No Garage
-
-        'Utilities':   ['None', 'ELO', 'NoSeWa', 'NoSewr', 'AllPub'],
-        'LotShape':    ['None', 'Reg', 'IR1', 'IR2', 'IR3'],
-        'LandSlope':   ['None', 'Gtl', 'Mod', 'Sev'],
-        'PavedDrive':  ['None', 'N', 'P', 'Y'],
-        'Functional':  ['None', 'Sal', 'Sev', 'Maj2', 'Maj1', 'Mod', 'Min2', 'Min1', 'Typ'],
-    
-
-        'Fence':        ['None', 'MnWw', 'GdWo', 'MnPrv', 'GdPrv'],  # ordre discutable, cf. remarque
-    }
     
     #On 'ordinalise' et on 'dummies' ce qui reste
 
